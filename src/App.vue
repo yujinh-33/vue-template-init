62637f6dd55e68efcf77a836fdd4367f13791e5e
yujinh-33 <yujinh.333@gmail.com>
2023-03-27 16:35:50 +0800

    新增element-plus、tailwindcss、normalize.css、sass

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="app flex justify-center">
+    <el-button>Submit</el-button>
+  </div>
+</template>
+
+<script setup lang="ts" name="app"></script>
+
+<style lang="scss" scoped></style>
